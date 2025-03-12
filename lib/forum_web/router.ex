aeaca51d413b0f9e4e73042853dd56a4b48b7af4
Yasser Batas - yasskate <yassk8@gmail.com>
2025-03-12 17:21:45 -0600

    Add get and post endpoints

--- lib/forum_web/router.ex
+++ lib/forum_web/router.ex
@@ -18,12 +18,16 @@ defmodule ForumWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/users", PageController, :users
   end
 
+
+
   # Other scopes may use custom stacks.
-  # scope "/api", ForumWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", ForumWeb do
+    pipe_through :api
+    resources "/posts", PostController, except: [:new, :edit]
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:forum, :dev_routes) do
